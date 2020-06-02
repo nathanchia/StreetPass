8c7ce5a452cac7394b9d389b42ba021090ab8e42
Nathan Chia <nkchia@uw.edu>
2020-06-03 04:38:55 +0800

    Started network

--- screens/EditProfileScreen.js
+++ screens/EditProfileScreen.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import InfoInput from '../components/InfoInput';
 
 export default () => {
   return (
-    <View></View>
+    <View>
+      <InfoInput field={'Hobbies'}/>
+    </View>
   )
 }
 
--- stacks/PassStack.js
+++ stacks/PassStack.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import Enticons from 'react-native-vector-icons/Entypo';
+import * as Location from 'expo-location';
 import * as Styles from '../styles/master';
 
 
@@ -9,6 +10,49 @@ import PassListScreen from '../screens/PassListScreen';
 import PingButton from '../components/PingButton';
 
 const PassStack = createStackNavigator();
+
+const testRequest = () => {
+  return fetch('http://10.0.2.2:5000/ping')
+    .then((response) => response.json())
+    .then((json) => {
+      console.log(json.dev);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+};
+
+async function testPost() {
+  let { status } = await Location.requestPermissionsAsync();
+  if (status !== 'granted') {
+    console.log('rejected');
+  } else {
+    try {
+      let {coords} = await Location.getCurrentPositionAsync({});
+      console.log('' + coords.latitude + ', ' + coords.longitude);
+      fetch('http://10.0.2.2:5000/ping', {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          name: 'Nathan',
+          latitude: coords.latitude,
+          longitude: coords.longitude,
+        })
+      }).then((response) => response.json())
+        .then((json) => {
+          console.log(json.test);
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    } catch (e) {
+      console.log(e);
+    }
+  } 
+};
 
 export default ({ navigation }) => {
   const[testInfo, setTestInfo] = useState([
@@ -33,7 +77,7 @@ export default ({ navigation }) => {
             },
             headerTintColor : 'black',
             headerLeft: () => (<Enticons style={{marginLeft : 10}} name={'menu'} size={30} onPress={() => {navigation.openDrawer()}}/>),
-            headerRight: () => (<PingButton onPress={() => {navigation.openDrawer()}} />),
+            headerRight: () => (<PingButton onPress={testPost} />),
           }} 
         />
       </PassStack.Navigator>
